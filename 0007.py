9cb3437655adf43b655152029b3a2284e04e5491
Jefferson Lisboa <108184841+JeffLisboa@users.noreply.github.com>
2023-04-07 21:26:06 -0300

    .

--- 0007.py
+++ 0007.py
@@ -1,4 +1,12 @@
-#Tuplas
+#Listas e tuplas.
+#OBS: os dados de uma lista os dados sao do mesmo tipo, já na tupla os dados podem ser de tipos distintos como string, int e float juntos
+
+#Lista utilizando o construtir list
+churrasco = list(["Carvão", "Contra filé", "Fraldinha", "Toucinho de barriga", "Linguiça"])
+print(churrasco)
+
+
+#Cria uma lista com as strings informadas
 frutas = (
     "laranja",
     "pera",
@@ -6,9 +14,22 @@ frutas = (
 )
 print(frutas)
 
+#Lista usando apenas colchetes sem ter que informar o construtor list
+convidados = ["joão", "Maria", "Dalva", "Bento"]
+print(convidados)
+#Tambem é possível fatiar uma lista(0 based)
+print (convidados[0])
+print (convidados[3])
+print (convidados[:3])
+print (convidados[2:])
+print (convidados[::-1])
+
+
+#A estrutura tuple vai separar todas as letras da string informada
 letras = tuple("python")
 print(letras)
 
+#Cria uma lista de numeros inteiros separados com as vírgulas já informadas sem acrescentar mais virgula na execução do código
 numeros = tuple([1, 2, 3, 4])
 print(numeros)
 
