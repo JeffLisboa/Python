562081941df9900dfa4b33105112215d061dac35
Jefferson Lisboa <108184841+JeffLisboa@users.noreply.github.com>
2023-04-07 22:34:36 -0300

    .

--- 0009.py
+++ 0009.py
@@ -1,10 +1,12 @@
 #Desafio básico formação python (Tweet)
 #variavel onde sao inseridos os dados do post e lê a quantidade de caracteres
-T = len(input("Faça um Tweet: "))
+T = (input("Faça um Tweet: "))
 
 #Será verificado se os dados inseridos em T são menores ou igual 140
-if T <= 140:
+if len (T) <= 140:
     print("TWEET")
 
 else:
     print("MUTE") 
+
+  
