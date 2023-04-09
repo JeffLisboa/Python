68d866b655b3c3b0917902e76a19b006297b780d
Jefferson Lisboa <108184841+JeffLisboa@users.noreply.github.com>
2023-04-09 14:46:13 -0300

    .

--- 0012.py
+++ 0012.py
@@ -10,10 +10,24 @@ pessoa = dict(nome="Jefferson", idade="33")
 pessoa["telefone"] = "31 97112-0374"
 
 #Para acessar aos dados desse dicionario, informamos o nome do dicionario e entre colchetes o nome do dado
-pessoa["nome"]
-pessoa["telefone"]
-pessoa["idade"]
-# Ou .get quando nao tiver certeza se a chave existe no dicionario
-pessoa.get["nome"]
-pessoa.get["telefone"]
-pessoa.get["idade"]
+print(pessoa["nome"])
+print(pessoa["idade"])
+print(pessoa["telefone"])
+
+#Para saber se um valor está dentro de um dicionario
+#Crie uma variavel onde pergunte se telefone está dentro (in) do dicionario pessoa
+consulta = "telefone" in pessoa
+#Imprima o resultado
+print(consulta)
+
+#Tabem podemos criar dicionarios aninhados
+contatos = {
+    "contato_1@gmail.com": {"nome": "contato_1", "telefone": "31 99999-1111"},
+    "contato_2@gmail.com": {"nome": "contato_2", "telefone": "31 99999-2222"},
+    "contato_3@gmail.com": {"nome": "contato_3", "telefone": "31 99999-3333"}
+}
+
+# Usar .get quando nao tiver certeza se a chave existe no dicionario
+tel= contatos.get("contato_1@gmail.com", {})
+print(tel)
+
